b112ca3e30116b88494614f908c041d75128430a
Jonatas Ferreira <jonatas.lima@ccc.ufcg.edu.br>
2020-11-14 13:03:26 -0300

    adicionando Cidade e Estado

--- src/main/java/com/jonatasferreira/demo/DemoApplication.java
+++ src/main/java/com/jonatasferreira/demo/DemoApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.jonatasferreira.demo.domain.Categoria;
+import com.jonatasferreira.demo.domain.Cidade;
+import com.jonatasferreira.demo.domain.Estado;
 import com.jonatasferreira.demo.domain.Produto;
 import com.jonatasferreira.demo.repositories.CategoriaRepository;
+import com.jonatasferreira.demo.repositories.CidadeRepository;
+import com.jonatasferreira.demo.repositories.EstadoRepository;
 import com.jonatasferreira.demo.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -21,6 +25,12 @@ public class DemoApplication implements CommandLineRunner {
 	@Autowired
 	ProdutoRepository produtoRepository;
 
+	@Autowired
+	EstadoRepository estadoRepository;
+	
+	@Autowired
+	CidadeRepository cidadeRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
@@ -34,6 +44,18 @@ public class DemoApplication implements CommandLineRunner {
 		Produto p2 = new Produto("Impressora", 800.0);
 		Produto p3 = new Produto("Mouse", 80.0);
 		
+		Estado e1 = new Estado("Minas Gerais"); 
+		Estado e2 = new Estado("São Paulo"); 
+		Estado e3 = new Estado("Paraíba"); 
+		
+		Cidade cid1 = new Cidade("Uberlândia", e1);
+		Cidade cid2 = new Cidade("São Paulo", e2);
+		Cidade cid3 = new Cidade("Campina Grande", e3);
+		
+		e1.adicionaCidades(Arrays.asList(cid1));
+		e2.adicionaCidades(Arrays.asList(cid2));
+		e3.adicionaCidades(Arrays.asList(cid3));
+		
 		c1.adicionaProdutos(Arrays.asList(p1, p2, p3));
 		c2.adicionaProdutos(Arrays.asList(p2));
 		
@@ -43,6 +65,8 @@ public class DemoApplication implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(c1, c2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		estadoRepository.saveAll(Arrays.asList(e1, e2, e3));
+		cidadeRepository.saveAll(Arrays.asList(cid1, cid2, cid3));
 	}
 
 }
